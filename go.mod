310bcfd54e88587c52fbe54543829b9eaf86d111
mopeneko <mopeneko@lem0n.cc>
2022-05-30 10:19:10 +0000

    unko

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/mopeneko/discord-mamadm
+
+go 1.18
+
+require (
+	github.com/andersfylling/disgord v0.35.1 // indirect
+	github.com/andersfylling/snowflake/v5 v5.0.1 // indirect
+	github.com/greymd/mamadm v0.0.0 // indirect
+	github.com/klauspost/compress v1.15.5 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	golang.org/x/net v0.0.0-20220526153639-5463443f8c37 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/andersfylling/disgord"
+	"github.com/greymd/mamadm/generator"
+)
+
+func main() {
+	client := disgord.New(disgord.Config{
+		BotToken: os.Getenv("DISCORD_TOKEN"),
+		Intents:  disgord.IntentGuildMessages,
+	})
+
+	defer client.Gateway().StayConnectedUntilInterrupted()
+
+	client.Gateway().
+		MessageCreate(func(s disgord.Session, event *disgord.MessageCreate) {
+			if event.Message.Content != "!mama" {
+				return
+			}
+
+			msg, err := generator.Generate(0)
+			if err != nil {
+				log.Printf("failed to generate: %+v", err)
+			}
+
+			_, err = event.Message.Reply(context.Background(), s, msg)
+			if err != nil {
+				log.Printf("failed to send: %+v", err)
+			}
+		})
+}
